91ff1b4a1b1fed443f3ca5c631d5bb0c34f79959
chao <xiaochao200@126.com>
2023-08-22 15:22:50 +0800

    add use0

---
+++ Ch13/usett0.cpp
@@ -0,0 +1,19 @@
+#include "tabtenn0.h"
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    TableTennisPlayer player1("Chuck", "Blizzard", true);
+    TableTennisPlayer player2("Tara", "Boomdea", false);
+    player1.Name();
+    if (player1.HasTable()) cout << ": has a table.\n";
+    else cout << ": has't a table.\n";
+    player2.Name();
+    if (player2.HasTable()) cout << ": has a table.\n";
+    else cout << ": has't a table.\n";
+
+    return 0;
+}
